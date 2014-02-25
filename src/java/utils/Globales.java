2c7171c0c9801d584fc0c536fd2e6d930f13f329
User <User@User-PC>
2014-02-25 19:12:49 +0100

    GLOBALES

---
+++ src/java/utils/Globales.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+/**
+ *
+ * @author User
+ */
+public class Globales {
+    public static final String URLFOTOMUSICO="";
+    public static final String URLFOTOINSTRUMENTO="";
+    
+}
